a8d96b2ee2032631ca8bfbe70585dadad30af60a
Akanksha Phatanagare <akankshapha1@gmail.com>
2023-08-29 16:31:13 +0530

    [updated] Ownedpolicy

--- src/main/java/com/insurance/homeInsurance/controller/OwnedPolicyController.java
+++ src/main/java/com/insurance/homeInsurance/controller/OwnedPolicyController.java
@@ -1,5 +1,56 @@
 package com.insurance.homeInsurance.controller;
 
-public class OwnedPolicyController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.insurance.homeInsurance.dao.OwnedPolicyRepository;
+import com.insurance.homeInsurance.entity.OwnedPolicy;
+import com.insurance.homeInsurance.entity.Property;
+import com.insurance.homeInsurance.exception.CustomerException;
+import com.insurance.homeInsurance.exception.OwnedPolicyException;
+import com.insurance.homeInsurance.service.OwnedPolicyService;
+
+@RestController
+public class OwnedPolicyController {
+	
+@Autowired
+OwnedPolicyService  ownedPolicyService ;
+//
+//@GetMapping("getownedpolicy/{id}")
+//public OwnedPolicy getOwnedPolicyByCustomerId(@PathVariable ("id") Integer id ) throws OwnedPolicyException{
+//	return this.ownedPolicyService.getOwnedPolicyByCustomerId(id);
+//	
+//}
+@GetMapping("/")
+
+public String getGreeting() {
+
+    return "Welcome";
+
+}
+
+@PostMapping("/createOwnedPolicy/{id}")
+
+ public OwnedPolicy createOwnedPolicyByCustomerId(@RequestBody OwnedPolicy ownedPolicy, @PathVariable("id") Integer id) throws OwnedPolicyException {
+
+ return this.ownedPolicyService.createOwnedPolicyByCustomerId(ownedPolicy, id) ;
+
+}
+
+     
+
+@GetMapping("/getAllOwnedPolicies/{custId}")
+
+ public List<OwnedPolicy> getOwnedPolicyByCustomerId(@PathVariable("custId") Integer CustId ) throws OwnedPolicyException{
+
+ return this.ownedPolicyService.getownedPolicyByCustomerId(CustId);
+
+
+ }
 }
