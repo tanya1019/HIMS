11221c897e542314664a123a76915440488bbcbf
tanya1019 <tanya.vanneri19@gmail.com>
2023-08-30 10:07:50 +0530

    Email send integration

--- src/main/java/com/insurance/homeInsurance/HomeInsuranceApplication.java
+++ src/main/java/com/insurance/homeInsurance/HomeInsuranceApplication.java
@@ -1,13 +1,30 @@
 package com.insurance.homeInsurance;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+
+import com.insurance.homeInsurance.service.EmailSenderService;
+
 
 @SpringBootApplication
 public class HomeInsuranceApplication {
+	
+	@Autowired
+	EmailSenderService senderService;
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(HomeInsuranceApplication.class, args);
 	}
+	
+	@EventListener(ApplicationReadyEvent.class)
+	
+	public void sendEmail() {
+		senderService.sendEmail("anish.dhawalikar@bajajfinserv.in", "Need Help!!!!!!!", "Please help me with the further process");
+	}
+	
 
 }
