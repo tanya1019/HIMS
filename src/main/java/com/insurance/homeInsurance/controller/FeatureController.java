c674324c85ba860b42c54c05ad8d55509c099147
Akanksha Phatanagare <akankshapha1@gmail.com>
2023-09-08 13:06:20 +0530

    feature

--- src/main/java/com/insurance/homeInsurance/controller/FeatureController.java
+++ src/main/java/com/insurance/homeInsurance/controller/FeatureController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.insurance.homeInsurance.entity.Feature;
 import com.insurance.homeInsurance.entity.OwnedPolicy;
+import com.insurance.homeInsurance.exception.CustomerException;
 import com.insurance.homeInsurance.exception.FeatureException;
 import com.insurance.homeInsurance.service.FeatureService;
 
@@ -41,7 +43,19 @@ public class FeatureController {
 		return this.featureService.getAllFeatures();
 	}
 	
-	
+	@PutMapping("/feature/{id}")
+
+    public Feature updateFeatureById(@RequestBody Feature feature ) throws FeatureException, CustomerException {
+
+        return this.featureService.updateFeature(feature);     
+
+ 
+
+    }
+
+    
+
+
 	
 	
 	@DeleteMapping("/delete/feature/{id}")
